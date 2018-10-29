38276f8e210ee4a9b0701928e62c9b2cd4e065e3
Röttger <mona.roettger@gmail.com>
2018-10-29 22:52:54 +0100

    insert dynamic Properties-Test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1> Hi I am a react app</h1>
+        <p>This is working</p>
+        <Recipe name="Levo" pieces="3"/>
+        <Recipe name="Panto" pieces="1">Anmerkung: Jeden morgen einnehmen</Recipe>
       </div>
     );
   }
---
+++ src/Recipe.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const recipe = (props) => {
+  return <p>"I am a {props.name}-Recipe with {props.pieces} pieces"</p>
+};
+
+export default recipe;
