5722eec604574e34ad5ed12f4d236f96ff9360c2
Röttger <mona.roettger@gmail.com>
2018-10-29 23:50:02 +0100

    add Button-Click-Behavior for tests, manipulating properties

--- src/App.js
+++ src/App.js
@@ -3,13 +3,40 @@ import './App.css';
 import Recipe from './Recipe';
 
 class App extends Component {
+
+  //kann in Klassen die extends Component haben, die Variablem setzen
+  state = {
+    //Array
+    recipes: [
+      {name: 'Levoceti', pieces: 3},
+      {name: 'Pantopra', pieces: 1},
+      {name: 'Pille', pieces: 25}
+    ],
+    otherState:'some other value'
+  }
+
+//Funktion anlegen für Behavior bei Klick
+  switchNameHandler = () => {
+    //console.log('Was clicked');
+    // DONT DO THIS: this.state.recipes[0] = 'Aspirin';
+    //nur recipes wird "geupdated", otherState bleibt wie es ist
+    this.setState({
+      recipes: [
+        {name: 'Aspirin', pieces: 3},
+        {name: 'Pantopra', pieces: 1},
+        {name: 'Pille', pieces: 25}
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi I am a react app</h1>
         <p>This is working</p>
-        <Recipe name="Levo" pieces="3"/>
-        <Recipe name="Panto" pieces="1">Anmerkung: Jeden morgen einnehmen</Recipe>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Recipe name={this.state.recipes[0].name} pieces={this.state.recipes[0].pieces}/>
+        <Recipe name={this.state.recipes[1].name} pieces={this.state.recipes[1].pieces}>Anmerkung: Jeden morgen einnehmen</Recipe> {/*Anmerkung ist childrenProperty, siehe Recipe.js*/}
       </div>
     );
   }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const recipe = (props) => {
-  return <p>"I am a {props.name}-Recipe with {props.pieces} pieces"</p>
+    return(
+       <div>
+          <p>"I am a {props.name}-Recipe with {props.pieces} pieces"</p>
+          <p>{props.children}</p>  {/*zeigt die Anmerkung:... an*/}
+       </div>
+
+    )
 };
 
 export default recipe;
