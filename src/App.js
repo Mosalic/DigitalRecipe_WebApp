2b9ee6eb4e749267ede732588f662aeb27259193
Röttger <mona.roettger@gmail.com>
2018-11-06 23:59:29 +0100

    First Access to database ready

--- src/App.js
+++ src/App.js
@@ -2,9 +2,32 @@ import React, { Component } from 'react';
 import './App.css'; //import stylesheets
 import ContentHandler from './ContentHandler'; //own Component
 import Header from './Header';
+import axios from 'axios';
 
 class App extends Component {
 
+/*componentDidMount(){
+  fetch('http://localhost/android_connect/testgetusers.php')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  })
+  .catch(err => console.log("FEEEHLER " + err));
+}*/
+
+componentDidMount(){
+  axios.get('http://localhost/android_connect/testgetusers.php')
+  .then((response) => {
+
+      console.log(response.data);
+
+  })
+  .catch((error) => {
+    console.log('error', error);
+  })
+}
 
   render() {
 
@@ -12,7 +35,7 @@ class App extends Component {
       return (
         <div className="App">
           <Header/>
-          <ContentHandler />
+          {/*<ContentHandler />*/}
 
         </div>
       );
